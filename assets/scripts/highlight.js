a0c5e20e213b2bde7968b568249f39eee792a4dd
Matheus Pazinati <matheuspazinati@gmail.com>
2021-12-16 18:11:20 -0300

    Finaliza feature highlightjs

--- assets/scripts/highlight.js
+++ assets/scripts/highlight.js
@@ -1,15 +1,55 @@
 const codeContainer = document.querySelector('.code__content')
 const buttonHighlight = document.querySelector('.button__highlight')
 const selectedLanguage = document.querySelector('.personalization__language')
-const codeTextArea = document.querySelector('.code__content textarea')
 
-function applyHighlight() {
-  const projectCode = codeTextArea.value
-  codeContainer.innerHTML = `<textarea name="code" id="code" spellcheck="false" class="hljs ${selectedLanguage.value}"></textarea>`
-  codeContainer.querySelector('textarea').textContent = projectCode
-  hljs.highlightElement(codeTextArea)
+const CodeEditor = {
+  codeContainer,
+  buttonHighlight,
+  selectedLanguage,
+  highlight(){
+    const projectCode = codeContainer.innerText 
+    codeContainer.innerHTML = `<code id="code" spellcheck="false" contenteditable="true" class="hljs ${selectedLanguage.value}"></code>`
+    codeContainer.querySelector('#code').textContent = projectCode
+    hljs.highlightElement(codeContainer.querySelector('#code'))
+  },
 }
 
-buttonHighlight.addEventListener('click', () => {
-  applyHighlight()
+function changeHighlight() {
+  if (CodeEditor.codeContainer.classList.contains('code__content--disabled')){
+    CodeEditor.highlight()
+  }
+}
+
+function applyHighlight() {
+  if (CodeEditor.codeContainer.classList.contains('code__content--disabled')){
+    return;
+  }
+  CodeEditor.highlight()
+}
+
+function disableCodeContainer() {
+  CodeEditor.codeContainer.classList.toggle('code__content--disabled')
+}
+
+function changeHighlightButton() {
+  CodeEditor.buttonHighlight.classList.toggle('button__highlight--remove')
+}
+
+function removeHighlight() {
+  const userText = CodeEditor.codeContainer.innerText
+  if (code.classList.contains("hljs")){
+    CodeEditor.codeContainer.innerHTML = `<code id="code" spellcheck="false" contenteditable="true" class="hljs"></code>`
+    code.textContent = userText
+  }
+}
+
+CodeEditor.buttonHighlight.addEventListener('click', () => {
+  removeHighlight()
+  changeHighlightButton();
+  applyHighlight();
+  disableCodeContainer();
+})
+
+selectedLanguage.addEventListener('change', () => {
+  changeHighlight()
 })
