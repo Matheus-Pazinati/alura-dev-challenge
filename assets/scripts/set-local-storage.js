997e27db04e49a55b65269d60a198f740e1a7942
Matheus Pazinati <matheuspazinati@gmail.com>
2022-08-23 03:58:56 -0300

    Separa codigo que salva os dados do projeto em um novo modulo

--- assets/scripts/set-local-storage.js
+++ assets/scripts/set-local-storage.js
@@ -1,70 +1,74 @@
-const projectName = document.querySelector('[data-project-name]')
-const projectDescription = document.querySelector('[data-project-description]')
-const projectBorderColor = document.querySelector('[data-project-colors]')
-const projectForm = document.querySelector('[data-form]')
-const projectLanguage = document.querySelector('[data-project-language]')
-const editorBorder = document.querySelector('.editor__container')
-const codeText = document.querySelector('.code__content')
+const CodeData = {
+  projectForm: document.querySelector('[data-form]'),
+  projectName: document.querySelector('[data-project-name]'),
+  projectDescription: document.querySelector('[data-project-description]'),
+  projectBorderColor: document.querySelector('[data-project-colors]'),
+  projectLanguage: document.querySelector('[data-project-language]'),
+  editorBorder: document.querySelector('.editor__container'),
+  codeText: document.querySelector('.code__content'),
 
-function saveOnLocalStorage() {
-  const projectData = {
-    code: codeText.innerHTML,
-    name: projectName.value,
-    description: projectDescription.value,
-    color: projectBorderColor.value,
-  }
-  const projectsList = JSON.parse(localStorage.getItem("projects")|| '[]');
-  projectsList.push(projectData);
-  localStorage.setItem("projects", JSON.stringify(projectsList)|| '[]');
-}
+  verifyLogin: () => {
+    const userLogged = JSON.parse(localStorage.getItem("user"));
+    if (userLogged == null) {
+      Swal.fire(
+        'Ops, está faltando alguma coisa',
+        'Você ainda não fez login na plataforma',
+        'error'
+      )
+      throw new Error('Falta fazer login')
+    }
+  },
 
-projectForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-  verifyLogin();
-  verifyEmptyCodeField();
-  saveOnLocalStorage();
-  showSuccessMessage();
-  clearProjectFields();
-})
-function verifyEmptyCodeField() {
-  if (codeText.innerText == ""){
+  verifyEmptyCodeField: () => {
+    if (CodeData.codeText.innerText == ""){
+      Swal.fire(
+        'Ops, está faltando alguma coisa',
+        'O campo de código está vazio!',
+        'error'
+      )
+      throw new Error('O campo de código está vazio')
+    }
+  },
+  showSucessMessage: () => {
     Swal.fire(
-      'Ops, está faltando alguma coisa',
-      'O campo de código está vazio!',
-      'error'
+      'Projeto salvo com sucesso!',
+      'Você pode visualizá-lo na página da Comunidade',
+      'success'
     )
-    throw new Error('O campo de código está vazio')
+  },
+
+  saveOnLocalStorage: () => {
+    const projectData = {
+      code: CodeData.codeText.innerHTML,
+      name: CodeData.projectName.value,
+      description: CodeData.projectDescription.value,
+      color: CodeData.projectBorderColor.value,
+    }
+    const projectsList = JSON.parse(localStorage.getItem("projects")|| '[]');
+    projectsList.push(projectData);
+    localStorage.setItem("projects", JSON.stringify(projectsList)|| '[]');
+  },
+
+  clearProjectFields: () => {
+    CodeData.codeText.innerHTML = `<pre><code id="code" spellcheck="false" contenteditable="true"></code></pre>`
+    CodeData.codeText.classList.remove('code__content--disabled');
+    CodeData.projectName.value = "";
+    CodeData.projectDescription.value = "";
+    CodeData.projectLanguage.selectedIndex = 0;
+    CodeData.projectBorderColor.value = "#0597F2"
+    CodeData.editorBorder.style.borderColor = "#0597F2";
+    document.querySelector('[data-export-button]').disabled = true;
+    document.querySelector('.button__highlight').classList.remove('button__highlight--remove');
   }
 }
 
-function showSuccessMessage() {
-  Swal.fire(
-    'Projeto salvo com sucesso!',
-    'Você pode visualizá-lo na página da Comunidade',
-    'success'
-  )
-}
-
-function clearProjectFields() {
-  codeText.innerHTML = `<pre><code id="code" spellcheck="false" contenteditable="true"></code></pre>`
-  codeText.classList.remove('code__content--disabled');
-  document.querySelector('.button__highlight').classList.remove('button__highlight--remove');
-  projectName.value = "";
-  projectDescription.value = "";
-  projectLanguage.selectedIndex = 0;
-  projectBorderColor.value = "#0597F2"
-  editorBorder.style.borderColor = "#0597F2";
-  document.querySelector('[data-export-button]').disabled = true;
-}
-
-function verifyLogin () {
-  const userLogged = JSON.parse(localStorage.getItem("user"));
-  if (userLogged == null) {
-    Swal.fire(
-      'Ops, está faltando alguma coisa',
-      'Você ainda não fez login na plataforma',
-      'error'
-    )
-    throw new Error('Falta fazer login')
-  }
+export function handleCodeData() {
+  CodeData.projectForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    CodeData.verifyLogin();
+    CodeData.verifyEmptyCodeField();
+    CodeData.saveOnLocalStorage();
+    CodeData.showSucessMessage();
+    CodeData.clearProjectFields();
+  })
 }
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import { handleCodeContainerBorderColor } from "./assets/scripts/change-border.js";
 import { handleMenuIconsOpenClose } from "./assets/scripts/open-close-menu.js";
 import { handleSearchFieldOpenClose } from "./assets/scripts/open-close-search.js";
+import { handleCodeData } from "./assets/scripts/set-local-storage.js";
 
 handleSearchFieldOpenClose();
 handleMenuIconsOpenClose();
 handleCodeContainerBorderColor();
+handleCodeData();
