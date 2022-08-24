f0169680f5751a6d7b9535335479475c82e8e3ce
Matheus Pazinati <matheuspazinati@gmail.com>
2022-08-24 04:30:51 -0300

    Cria modulo para codigo de dar like no projeto

--- assets/scripts/like-system.js
+++ assets/scripts/like-system.js
@@ -1,19 +1,21 @@
-const likeButtons = document.querySelectorAll('[data-like-button]')
+export function handleProjectLike() {
+  const likeButtons = document.querySelectorAll('[data-like-button]')
 
-likeButtons.forEach((likeButton) => {
-  let projectLiked = false;
-
-  likeButton.addEventListener('click', () => {
-    let likeNumber = Number(likeButton.innerText);
-    let likeCount = likeButton.querySelector('span');
-    if (projectLiked) {
-      likeCount.textContent = likeNumber-= 1;
-      likeButton.children[0].style.color = "#FEFEFE";
-      projectLiked = false;
-    }else {
-      likeCount.textContent = likeNumber+= 1;
-      likeButton.children[0].style.color = "#F65151";
-      projectLiked = true;
-    }
+  likeButtons.forEach((likeButton) => {
+    let projectLiked = false;
+  
+    likeButton.addEventListener('click', () => {
+      let likeNumber = Number(likeButton.innerText);
+      let likeCount = likeButton.querySelector('span');
+      if (projectLiked) {
+        likeCount.textContent = likeNumber-= 1;
+        likeButton.children[0].style.color = "#FEFEFE";
+        projectLiked = false;
+      }else {
+        likeCount.textContent = likeNumber+= 1;
+        likeButton.children[0].style.color = "#F65151";
+        projectLiked = true;
+      }
+    })
   })
-})
+}
