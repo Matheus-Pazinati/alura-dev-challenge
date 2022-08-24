701cecd3214610b289adce879b8ad9b8f96e6bfe
Matheus Pazinati <matheuspazinati@gmail.com>
2022-08-24 04:23:52 -0300

    Cria modulo para o codigo que pega os dados no local storage e mostra na tela

--- assets/scripts/get-local-storage.js
+++ assets/scripts/get-local-storage.js
@@ -1,43 +1,45 @@
-const projectsList = JSON.parse(localStorage.getItem("projects")|| '[]');
-const projectsContainer = document.querySelector('[data-project-container]')
-
-projectsList.forEach((project, index) => {
-  const projectItem = document.createElement('li');
-  projectItem.classList.add('editor__container')
-  projectItem.innerHTML = ` <div class="editor__content editor__content-disabled">
-  <div class="editor__icons">
-    <div class="editor__icon icon-red"></div>
-    <div class="editor__icon icon-yellow"></div>
-    <div class="editor__icon icon-green"></div>
-  </div>
-  <label for="code" class="sr-only">Editor de código</label>
-  <div class="code__content">
-    <pre><code id="code" spellcheck="false" contenteditable="false"></code></pre>
-  </div>
-  </div>
-  <div class="project__data">
-    <h2 class="project__title">${project.name}</h2>
-    <p class="project__description">${project.description}</p>
-    <div class="project__status">
-      <div class="project__status-button">
-        <button class="project__button">
-          <i class="icon-comment" aria-label="Comentar"></i>
-          0
-        </button>
-        <button data-like-button class="project__button">
-          <i class="icon-heart" aria-label="Curtir"></i>
-          <span>0</span>
-        </button>
-      </div>
-      <a class="profile__link">
-        <img>
-        <span></span>
-      </a>
+export function getProjectDataFromLocalStorage() {
+  const projectsList = JSON.parse(localStorage.getItem("projects")|| '[]');
+  const projectsContainer = document.querySelector('[data-project-container]')
+  
+  projectsList.forEach((project, index) => {
+    const projectItem = document.createElement('li');
+    projectItem.classList.add('editor__container')
+    projectItem.innerHTML = ` <div class="editor__content editor__content-disabled">
+    <div class="editor__icons">
+      <div class="editor__icon icon-red"></div>
+      <div class="editor__icon icon-yellow"></div>
+      <div class="editor__icon icon-green"></div>
     </div>
-  </div>`;
-  projectsContainer.appendChild(projectItem);
-  const projectCodeContent = projectsContainer.querySelectorAll('.code__content');
-  projectCodeContent[index].innerHTML = project.code;
-  const projectCodeBorder = document.querySelectorAll('.editor__content');
-  projectCodeBorder[index].style.borderColor = project.color;
-})
+    <label for="code" class="sr-only">Editor de código</label>
+    <div class="code__content">
+      <pre><code id="code" spellcheck="false" contenteditable="false"></code></pre>
+    </div>
+    </div>
+    <div class="project__data">
+      <h2 class="project__title">${project.name}</h2>
+      <p class="project__description">${project.description}</p>
+      <div class="project__status">
+        <div class="project__status-button">
+          <button class="project__button">
+            <i class="icon-comment" aria-label="Comentar"></i>
+            0
+          </button>
+          <button data-like-button class="project__button">
+            <i class="icon-heart" aria-label="Curtir"></i>
+            <span>0</span>
+          </button>
+        </div>
+        <a class="profile__link">
+          <img>
+          <span></span>
+        </a>
+      </div>
+    </div>`;
+    projectsContainer.appendChild(projectItem);
+    const projectCodeContent = projectsContainer.querySelectorAll('.code__content');
+    projectCodeContent[index].innerHTML = project.code;
+    const projectCodeBorder = document.querySelectorAll('.editor__content');
+    projectCodeBorder[index].style.borderColor = project.color;
+  })
+}
--- community.js
+++ community.js
@@ -1,7 +1,9 @@
 import { handleMenuIconsOpenClose } from "./assets/scripts/open-close-menu.js";
 import { handleSearchFieldOpenClose } from "./assets/scripts/open-close-search.js";
 import { showUserProfile } from "./assets/scripts/show-profile.js";
+import { getProjectDataFromLocalStorage } from './assets/scripts/get-local-storage.js';
 
 handleMenuIconsOpenClose();
 handleSearchFieldOpenClose();
 showUserProfile();
+getProjectDataFromLocalStorage();
